aee89953fd86e7c5957c7668b3e0c2617d245594
Adrian Horning <m.adrian.horning@gmail.com>
2018-03-24 16:27:45 -0700

    firstDuplicate)

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-if (false) {
-  console.log('hey');
+a: [2, 3, 3, 1, 5, 2];
+
+
+function firstDuplicate(a) {
+  for (let i of a) {
+    let posi = Math.abs(i) - 1
+    if (a[posi] < 0) return posi + 1
+    a[posi] = a[posi] * -1
+  }
+
+  return -1
 }
+
+firstDuplicate(a);
