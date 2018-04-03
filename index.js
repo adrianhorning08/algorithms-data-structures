58ea6a471cc423f61271001e9625b6c31add0a8f
Adrian Horning <m.adrian.horning@gmail.com>
2018-04-03 16:39:49 -0700

    Workfront qs

--- index.js
+++ index.js
@@ -11,7 +11,24 @@ function reverse(str) {
 
 // find the common numbers in 2 arrays and return them
 // [8,4,2,7], [9,0,4,2,8] -> [2,4,8]
-
+function common(arr1, arr2) {
+  var results = [];
+  var shortest;
+  var longest;
+  if (arr1.length <= arr2.length) {
+    shortest = arr1;
+    longest = arr2;
+  } else {
+    longest = arr2;
+    shortest = arr1;
+  }
+  for (var i = 0; i < shortest.length; i++) {
+    if (longest.includes(shortest[i])) {
+      results.push(shortest[i]);
+    }
+  }
+  return results;
+}
 
 
 // write a function that takes 2 parameters, S and T
@@ -40,3 +57,43 @@ function myInterpolate(S,T) {
   }
   return newStr;
 }
+
+// write a function that takes a string and performs the math operation
+// it should only take 2 operands and 1 operator
+// will only be (+ - / *)
+// for example ("4 + 5") -> 9 or ("-3 - 10") -> -13
+
+function solution(str) {
+  var operandsCounter = 0;
+  var op1;
+  var op2;
+  var operatorCounter = 0;
+  var operator;
+  str = str.split(' ');
+
+  for (var i = 0; i < str.length; i++) {
+    var curr = str[i];
+    if (isNaN(curr)) {
+      operatorCounter++;
+      operator = curr;
+    } else {
+      operandsCounter++;
+      op1 === undefined ? op1 = Number(curr) : op2 = Number(curr);
+    }
+  }
+
+  if (operandsCounter > 2 || operator > 1) return false;
+
+  switch (operator) {
+    case '+':
+      return op1 + op2;
+    case '-':
+      return op1 - op2;
+    case '/':
+      return op1 / op2;
+    case '*':
+      return op1 * op2;
+  }
+}
+
+console.log(solution("5 * 10"));
