e438f809f0d0218ae7813b473515b5ce335b24f3
Adrian Horning <m.adrian.horning@gmail.com>
2018-04-03 15:54:36 -0700

    Workfront qs

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-// var nums = [-1, 0, 1, 2, -1, -4];
-var nums = [-8, -7, 5, 2];
 
-// [
-//   [-1, 0, 1],
-//   [-1, -1, 2]
-// ]
+// Workfront questions
+// reverse a string
+function reverse(str) {
+  var newStr = '';
+  for (var i = str.length-1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+}
 
-var threeSum = function(nums) {
-   var results = [];
-   nums = nums.sort();
-   for (var i = 0; i < nums.length-2; i++) {
-     var currNum = nums[i];
+// find the common numbers in 2 arrays and return them
+// [8,4,2,7], [9,0,4,2,8] -> [2,4,8]
 
-     var j = i+1;
-     var k = nums.length -1;
-     while (j < k) {
-       if (currNum + nums[j] + nums[k] === 0) {
-         results.push([currNum, nums[j], nums[k]]);
-         j++;
-         k--;
-       } else if (currNum + nums[j] + nums[k] < 0) {
-         j++;
-       } else if (currNum + nums[j] + nums[k] > 0) {
-         k--;
-       }
-     }
-   }
-   return results;
-};
 
-console.log(threeSum(nums));
+
+// write a function that takes 2 parameters, S and T
+// S will be a string, for example "Hello ${val1} and ${val2}!"
+// T will be a string, for example "{"val1":"Sarah","val2":"Jane"}"
+// your output should return "Hello Sarah and Jane!"
+
+function myInterpolate(S,T) {
+  T = JSON.parse(T);
+  S = S.replace(/\$/g,'');
+  var newStr = '';
+  for (var i = 0; i < S.length; i++) {
+    var curr = S[i];
+    if (curr !== '{') {
+      newStr += curr;
+    } else {
+      var j = i+1;
+      var temp = '';
+      while (S[j] !== '}') {
+        temp += S[j];
+        j++;
+      }
+      i = j;
+      newStr += T[temp];
+    }
+  }
+  return newStr;
+}
