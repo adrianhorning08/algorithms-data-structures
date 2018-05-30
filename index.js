42a0491149aebfd1681cc3ecbefc71e495c97384
Adrian Horning <m.adrian.horning@gmail.com>
2018-05-30 12:15:01 -0700

    comment

--- index.js
+++ index.js
@@ -69,16 +69,27 @@ Array.prototype.inRange = function(min, max) {
 }
 
 
-// You've been tasked with writing an autocorrect service for messages sent by your legal team. The messages which are sent to other lawyers need to be updated so that each message sent references the lawyer's client for clarity. To do this you need to replace all instances of "you" and it's misspellings with "your client".
-// Write a function called autocorrect that takes a string and replaces all instances of "you", "youuu", or "u" (not case sensitive) with "your client" (always lower case).
+// You've been tasked with writing an autocorrect service for messages sent by your legal team.
+// The messages which are sent to other lawyers need to be updated so that each message sent
+// references the lawyer's client for clarity. To do this you need to replace all instances of
+// "you" and it's misspellings with "your client".
+// Write a function called autocorrect that takes a string and replaces all instances of
+// "you", "youuu", or "u" (not case sensitive) with "your client" (always lower case).
 // Return the resulting string.
-// Here's the slightly tricky part: These are informal messages, so there are different forms of "you" and "u".
+// Here's the slightly tricky part: These are informal messages, so there are different
+// forms of "you" and "u".
 // For the purposes of this challenge, here's what you need to support:
 //
 // "youuuuu" with any number of u characters tacked onto the end
 // "u" at the beginning, middle, or end of a string, but NOT part of a word
 // "you" but NOT as part of another word like "young" or "Bayou"
 //
+
+function autocorrect() {
+
+}
+
+console.log(autocorrect('youtube'));
 
 
 
